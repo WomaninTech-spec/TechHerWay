30d119abc978694488cd3196c7fbb35f7d51be4b
Barbara Teslar <83292442+WomaninTech-spec@users.noreply.github.com>
2026-05-21 17:14:01 +0200

    fix: update base path to /TechHerWay/ after repo rename

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <BrowserRouter basename="/TechHerWay-app">
+      <BrowserRouter basename="/TechHerWay">
         <Routes>
           {/* Public */}
           <Route path="/" element={<LandingPage />} />
